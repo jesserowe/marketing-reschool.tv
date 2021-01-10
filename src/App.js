806a016ae56b3c27f96e929681a824df5ce218e4
Justin Gottshall <justin.gottshall@sherwin.com>
2021-01-10 12:46:49 -0500

    remove unused useRef so netlify will build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import YouTube from 'react-youtube'
 import channels from './channels.json'
 import logo from './images/logo.png'
