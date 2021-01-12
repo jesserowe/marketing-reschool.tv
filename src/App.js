392eb44a1a7155f7eda53c41b67c9a3a8dcdf2b1
Justin Gottshall <justin.gottshall@sherwin.com>
2021-01-11 22:34:57 -0500

    fix author not showing, implement youtube link button and volume slider

--- src/App.js
+++ src/App.js
@@ -2,47 +2,67 @@ import React, { useEffect, useState, useRef } from 'react'
 import YouTube from 'react-youtube'
 import channels from './channels.json'
 import logo from './images/logo.png'
+import throttle from 'lodash/throttle'
 
 function App() {
   const progressBar = useRef()
+  const volumeSlideBar = useRef()
 
-  const [videoControls, setVideoControls] = useState() // mute, unMute, isMuted, getVolume, playVideo, pauseVideo, getDuration, 
-  const [activeCategoryIndex, setActiveCategoryIndex] = useState(0)
-  const [videoIndex, setVideoIndex] = useState(Math.floor(Math.random() * channels[activeCategoryIndex].videoIds.length))
-  const [videoId, setVideoId] = useState(channels[activeCategoryIndex].videoIds[videoIndex])
-  const [currentVideoTitle, setCurrentVideoTitle] = useState()
-  const [currentVideoAuthor, setCurrentVideoAuthor] = useState()
+  const [videoControls, setVideoControls] = useState() // mute, unMute, isMuted, getVolume, playVideo, pauseVideo, getDuration, playerInfo
+  const [activeChannel, setActiveChannel] = useState(0)
+  const [videoIndex, setVideoIndex] = useState(Math.floor(Math.random() * channels[activeChannel].videoIds.length))
+  const [videoId, setVideoId] = useState(channels[activeChannel].videoIds[videoIndex])
+  const [videoData, setVideoData] = useState()
   const [isMuted, setIsMuted] = useState(true)
   const [isPlaying, setIsPlaying] = useState(true)
   const [videoProgress, setVideoProgress] = useState(0)
+  // const [videoProgressChanging, setVideoProgressChanging] = useState(false)
   const [sidePannelShowing, setSidePannelShowing] = useState(true)
+  const [youtubeLinkShowing, setYoutubeLinkShowing] = useState(false)
+  const [volumeControlOpen, setVolumeControlOpen] = useState(false)
+  const [volume, setVolume] = useState(0)
+  const [volumeChanging, setVolumeChanging] = useState(false)
 
-  useEffect(() => {
-    const intervalId = setInterval(() => videoControls && setVideoProgress(videoControls.playerInfo.currentTime / videoControls.getDuration() * 100), 50)
+  useEffect(() => { // update the progressBar progress every 50 ms
+    const intervalId = setInterval(() => {
+      videoControls && setVideoProgress(videoControls.playerInfo.currentTime / videoControls.getDuration() * 100)
+    }, 50)
     return () => clearTimeout(intervalId)
   })
 
-  useEffect(() => {
-    setVideoIndex(Math.floor(Math.random() * channels[activeCategoryIndex].videoIds.length))
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => videoControls && setVideoData(videoControls.getVideoData()), [videoControls && videoControls.getVideoData()])
+
+  useEffect(() => { // start playing videos from random index when channel is changed
+    setVideoIndex(Math.floor(Math.random() * channels[activeChannel].videoIds.length))
     setIsPlaying(true)
-  }, [activeCategoryIndex])
+  }, [activeChannel])
 
-  useEffect(() => { setVideoId(channels[activeCategoryIndex].videoIds[videoIndex]) }, [videoIndex, activeCategoryIndex])
+  useEffect(() => setVideoId(channels[activeChannel].videoIds[videoIndex]), [videoIndex, activeChannel])
 
-  useEffect(() => { videoControls && (isMuted ? videoControls.mute() : videoControls.unMute()) }, [videoControls, isMuted])
+  useEffect(() => videoControls && (isMuted ? videoControls.mute() : videoControls.unMute()), [videoControls, isMuted])
 
-  const nextVideo = () => setVideoIndex((videoIndex + 1) % channels[activeCategoryIndex].videoIds.length)
-  const prevVideo = () => setVideoIndex(videoIndex === 0 ? channels[activeCategoryIndex].videoIds.length : videoIndex - 1)
+  const nextVideo = () => setVideoIndex((videoIndex + 1) % channels[activeChannel].videoIds.length)
+  const prevVideo = () => setVideoIndex(videoIndex === 0 ? channels[activeChannel].videoIds.length : videoIndex - 1)
 
   const remainingSeconds = videoControls ? Math.floor(videoControls.getDuration() - videoControls.playerInfo.currentTime) : 0
+
   const remainingMinutes = Math.floor(remainingSeconds / 60)
+
+  const updateVolume = throttle((pageY) => {
+    const { bottom } = volumeSlideBar.current.getBoundingClientRect()
+    const newVolume = Math.max(Math.min(Math.round(bottom - pageY), 100), 0)
+    videoControls.setVolume(newVolume)
+    setVolume(newVolume)
+    setIsMuted(newVolume === 0)
+  }, 50)
 
   return (
     <div className='flex w-screen'>
       {/* video player */}
       <div id='video-player' className='relative flex-grow flex-shrink h-screen'>
         {/* mute button container */}
-        <div className='absolute w-full h-28 bg-black'>
+        <div className='absolute w-full h-24 bg-black'>
           {videoControls && isMuted && (
             <button
               className='bg-white rounded-md w-48 h-12 m-5 flex items-center justify-evenly font-bold'
@@ -62,18 +82,18 @@ function App() {
           onEnd={nextVideo}
           onPlay={() => setIsPlaying(true)}
           onPause={() => setIsPlaying(false)}
-          onReady={({ target }) => {
-            const { author, title } = target.getVideoData()
-            setCurrentVideoTitle(title)
-            setCurrentVideoAuthor(author)
-            setVideoControls(target)
-          }}
+          onReady={({ target }) => setVideoControls(target)}
         />
         {/* player controls */}
         <div className='absolute bottom-0 w-full h-40 bg-black'>
           {videoControls && <div className='absolute bottom-0 left-0 right-0 m-3 rounded-lg bg-gray-1000 text-white p-2'>
-            {currentVideoAuthor && <p className='font-bold p-1'>{currentVideoAuthor}</p>}
-            {currentVideoTitle && <p className='p-1'>{currentVideoTitle}</p>}
+            {youtubeLinkShowing && ( // youtube link
+              <div className='absolute bg-submenu-gray w-60 top-4 right-4 rounded-md p-4 text-sm'>
+                <a href={`https://www.youtube.com/watch?v=${videoId}`} target='_blank' rel='noreferrer' className='opacity-70 hover:opacity-100'>Watch on YouTube</a>
+              </div>
+            )}
+            <p className='font-bold p-1 h-8'>{videoData && videoData.author}</p>
+            <p className='p-1 h-8'>{videoData && videoData.title}</p>
             <div className='flex p-1 items-center'>
               <button className='w-10 h-10 opacity-70 hover:opacity-100 focus:outline-none' onClick={prevVideo}> {/* back button */}
                 <svg className='fill-current w-7 h-7' viewBox="0 0 24 24"><path d="M7 6c.55 0 1 .45 1 1v10c0 .55-.45 1-1 1s-1-.45-1-1V7c0-.55.45-1 1-1zm3.66 6.82l5.77 4.07c.66.47 1.58-.01 1.58-.82V7.93c0-.81-.91-1.28-1.58-.82l-5.77 4.07a1 1 0 000 1.64z" /></svg>
@@ -95,24 +115,45 @@ function App() {
               <button className='w-10 h-10 opacity-70 hover:opacity-100 focus:outline-none' onClick={nextVideo}> {/* next button */}
                 <svg className='fill-current w-7 h-7' viewBox="0 0 24 24"><path d="M7.58 16.89l5.77-4.07c.56-.4.56-1.24 0-1.63L7.58 7.11C6.91 6.65 6 7.12 6 7.93v8.14c0 .81.91 1.28 1.58.82zM16 7v10c0 .55.45 1 1 1s1-.45 1-1V7c0-.55-.45-1-1-1s-1 .45-1 1z" /></svg>
               </button>
-              <button
-                className='w-10 h-10 opacity-70 hover:opacity-100 focus:outline-none'
-                onClick={() => {
-                  isMuted ? videoControls.unMute() : videoControls.mute()
-                  setIsMuted(!isMuted)
-                  // TODO: volume control widget
-                }}
+              <div
+                className='relative'
+                onMouseEnter={() => setVolumeControlOpen(true)}
+                onMouseLeave={() => setVolumeControlOpen(false)}
               > {/* volume button */}
-                <svg className='fill-current w-7 h-7' viewBox="0 0 24 24">
-                  {isMuted
-                    ? <path d="M3.63 3.63a.996.996 0 000 1.41L7.29 8.7 7 9H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71v-4.17l4.18 4.18c-.49.37-1.02.68-1.6.91-.36.15-.58.53-.58.92 0 .72.73 1.18 1.39.91.8-.33 1.55-.77 2.22-1.31l1.34 1.34a.996.996 0 101.41-1.41L5.05 3.63c-.39-.39-1.02-.39-1.42 0zM19 12c0 .82-.15 1.61-.41 2.34l1.53 1.53c.56-1.17.88-2.48.88-3.87 0-3.83-2.4-7.11-5.78-8.4-.59-.23-1.22.23-1.22.86v.19c0 .38.25.71.61.85C17.18 6.54 19 9.06 19 12zm-8.71-6.29l-.17.17L12 7.76V6.41c0-.89-1.08-1.33-1.71-.7zM16.5 12A4.5 4.5 0 0014 7.97v1.79l2.48 2.48c.01-.08.02-.16.02-.24z" />
-                    : <path d="M3 10v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71V6.41c0-.89-1.08-1.34-1.71-.71L7 9H4c-.55 0-1 .45-1 1zm13.5 2A4.5 4.5 0 0014 7.97v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 4.45v.2c0 .38.25.71.6.85C17.18 6.53 19 9.06 19 12s-1.82 5.47-4.4 6.5c-.36.14-.6.47-.6.85v.2c0 .63.63 1.07 1.21.85C18.6 19.11 21 15.84 21 12s-2.4-7.11-5.79-8.4c-.58-.23-1.21.22-1.21.85z" />
-                  }
-                </svg>
-              </button>
+                {volumeControlOpen && (
+                  <button
+                    className='absolute bottom-full left-0 h-28 w-7 bg-submenu-gray rounded-md focus:outline-none'
+                    onMouseUp={(e) => setVolumeChanging(false)}
+                    onMouseLeave={(e) => setVolumeChanging(false)}
+                    onMouseDown={(e) => setVolumeChanging(true)}
+                    onClick={(e) => updateVolume(e.pageY)}
+                    onMouseMove={(e) => volumeChanging && updateVolume(e.pageY)}
+                  >
+                    <div ref={volumeSlideBar} className='w-1 h-4/6 m-auto bg-volume-slider-gray relative focus:outline-none'>
+                      <div className={`absolute w-1 left-0 bottom-0 bg-white transition-height ${volumeChanging ? '' : 'duration-500'}`} style={{ height: `${volume}%` }} />
+                      <span className={`absolute h-3 w-3 rounded-lg bg-white -right-1 transition-bottom ${volumeChanging ? '' : 'duration-500'}`} style={{ bottom: `${volume}%` }} />
+                    </div>
+                  </button>
+                )}
+                <button
+                  className='w-10 h-10 opacity-70 hover:opacity-100 focus:outline-none'
+                  onClick={() => {
+                    isMuted ? videoControls.unMute() : videoControls.mute()
+                    isMuted ? setVolume(videoControls.getVolume()) : setVolume(0)
+                    setIsMuted(!isMuted)
+                  }}
+                >
+                  <svg className='fill-current w-7 h-7' viewBox="0 0 24 24">
+                    {isMuted
+                      ? <path d="M3.63 3.63a.996.996 0 000 1.41L7.29 8.7 7 9H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71v-4.17l4.18 4.18c-.49.37-1.02.68-1.6.91-.36.15-.58.53-.58.92 0 .72.73 1.18 1.39.91.8-.33 1.55-.77 2.22-1.31l1.34 1.34a.996.996 0 101.41-1.41L5.05 3.63c-.39-.39-1.02-.39-1.42 0zM19 12c0 .82-.15 1.61-.41 2.34l1.53 1.53c.56-1.17.88-2.48.88-3.87 0-3.83-2.4-7.11-5.78-8.4-.59-.23-1.22.23-1.22.86v.19c0 .38.25.71.61.85C17.18 6.54 19 9.06 19 12zm-8.71-6.29l-.17.17L12 7.76V6.41c0-.89-1.08-1.33-1.71-.7zM16.5 12A4.5 4.5 0 0014 7.97v1.79l2.48 2.48c.01-.08.02-.16.02-.24z" />
+                      : <path d="M3 10v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71V6.41c0-.89-1.08-1.34-1.71-.71L7 9H4c-.55 0-1 .45-1 1zm13.5 2A4.5 4.5 0 0014 7.97v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 4.45v.2c0 .38.25.71.6.85C17.18 6.53 19 9.06 19 12s-1.82 5.47-4.4 6.5c-.36.14-.6.47-.6.85v.2c0 .63.63 1.07 1.21.85C18.6 19.11 21 15.84 21 12s-2.4-7.11-5.79-8.4c-.58-.23-1.21.22-1.21.85z" />
+                    }
+                  </svg>
+                </button>
+              </div>
               <button
                 ref={progressBar}
-                className='flex-grow bg-white relative bg-progress-bar-gray h-1 focus:outline-none'
+                className='flex-grow relative bg-progress-bar-gray h-1 focus:outline-none'
                 onClick={(e) => {
                   // TODO: figure out why the target's bounding rectangle changes each time this is triggered
                   const { left, width } = progressBar.current.getBoundingClientRect()
@@ -144,11 +185,8 @@ function App() {
                   }
                 </svg>
               </button>
-              <button className='w-10 h-10 opacity-70 hover:opacity-100 focus:outline-none' onClick={() => { }}>
+              <button className='w-10 h-10 opacity-70 hover:opacity-100 focus:outline-none' onClick={() => setYoutubeLinkShowing(!youtubeLinkShowing)}>
                 <svg className='fill-current w-7 h-7' viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none" /><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" /></svg>
-                {/* <div className='relative bg-submenu-gray w-32 -top-20'>
-                  <a href='#'>Watch on YouTube</a>
-                </div> */}
               </button>
             </div>
           </div>}
@@ -167,9 +205,9 @@ function App() {
               ? ( // wide button
                 <button
                   key={index}
-                  className={`rounded-lg m-2 flex items-center text-white col-span-3 h-36 relative overflow-hidden border-white focus:outline-none ${index === activeCategoryIndex ? 'border-2' : ''}`}
+                  className={`rounded-lg m-2 flex items-center text-white col-span-3 h-36 relative overflow-hidden border-white focus:outline-none ${index === activeChannel ? 'border-2' : ''}`}
                   style={{ background }}
-                  onClick={() => setActiveCategoryIndex(index)}
+                  onClick={() => setActiveChannel(index)}
                 >
                   <div className='text-left w-8/12 m-5'>
                     {title && <p className='font-bold m-1 text-xl'>{title}</p>}
@@ -181,9 +219,9 @@ function App() {
               : ( // narrow button
                 <button
                   key={index}
-                  className={`rounded-lg m-2 text-center flex items-center text-white flex-col justify-center h-48 border-white focus:outline-none ${index === activeCategoryIndex ? 'border-2' : ''}`}
+                  className={`rounded-lg m-2 text-center flex items-center text-white flex-col justify-center h-48 border-white focus:outline-none ${index === activeChannel ? 'border-2' : ''}`}
                   style={{ background }}
-                  onClick={() => setActiveCategoryIndex(index)}
+                  onClick={() => setActiveChannel(index)}
                 >
                   {icon && <img className='w-14 h-14' src={`${process.env.PUBLIC_URL}/assets/${icon}`} alt='' />}
                   {title && <p className='font-bold m-1 w-10/12'>{title}</p>}
@@ -192,9 +230,6 @@ function App() {
               )
           ))}
         </div>
-        {/* <div className='text-white text-center opacity-70 m-5'>
-          Icons made by <a href='https://www.flaticon.com/free-icon/history_2234770?related_item_id=2234770&term=history' title='monkik'>monkik</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a>
-        </div> */}
       </div>}
     </div >
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         'gray-1000': 'rgba(34, 34, 34, 0.9)',
         'submenu-gray': 'rgb(64, 64, 64)',
         'progress-bar-gray': '#404040',
+        'volume-slider-gray': '#606060',
         'near-black': '#202020'
       },
       width: { '480': '480px' },
