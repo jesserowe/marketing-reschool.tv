81815faffc14b4cb6c6023c2829ef12272c3a3ac
Justin Gottshall <justin.gottshall@sherwin.com>
2021-05-15 21:20:32 -0400

    some adjustments to allow building with SSR

--- src/components/PlayerControls.js
+++ src/components/PlayerControls.js
@@ -35,6 +35,8 @@ const PlayerControls = ({
     return () => clearTimeout(intervalId)
   })
 
+  const [isFullscreen, setIsFullScreen] = useState(false)
+
   const remainingSeconds = controls ? Math.floor(controls.getDuration() - controls.playerInfo.currentTime) : 0
   const remainingMinutes = Math.floor(remainingSeconds / 60)
 
@@ -216,14 +218,13 @@ const PlayerControls = ({
             </button>
             <button
               className="w-10 h-10 opacity-70 hover:opacity-100 focus:outline-none"
-              onClick={() =>
-                document.fullscreenElement
-                  ? document.exitFullscreen()
-                  : document.getElementById("video-player").requestFullscreen()
-              }
+              onClick={() => {
+                isFullscreen ? document.exitFullscreen() : document.getElementById("video-player").requestFullscreen()
+                setIsFullScreen(!isFullscreen)
+              }}
             >
               <svg className="fill-current w-7 h-7" viewBox="0 0 24 24">
-                {document.fullscreenElement ? (
+                {isFullscreen ? (
                   <>
                     <path d="M0 0h24v24H0V0z" fill="none" />
                     <path d="M6 16h2v2c0 .55.45 1 1 1s1-.45 1-1v-3c0-.55-.45-1-1-1H6c-.55 0-1 .45-1 1s.45 1 1 1zm2-8H6c-.55 0-1 .45-1 1s.45 1 1 1h3c.55 0 1-.45 1-1V6c0-.55-.45-1-1-1s-1 .45-1 1v2zm7 11c.55 0 1-.45 1-1v-2h2c.55 0 1-.45 1-1s-.45-1-1-1h-3c-.55 0-1 .45-1 1v3c0 .55.45 1 1 1zm1-11V6c0-.55-.45-1-1-1s-1 .45-1 1v3c0 .55.45 1 1 1h3c.55 0 1-.45 1-1s-.45-1-1-1h-2z" />
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,17 @@ import { navigate } from "gatsby"
 import { sample } from "lodash"
 import channels from "../../data/channels.json"
 
+const isBrowser = typeof window !== "undefined"
+
 const IndexPage = () => {
-  console.log('index')
+  console.log("index")
   // TODO: uncomment when all channels have videos
   // const randomChannel = sample(channels)
   const randomChannel = channels[0]
 
-  navigate(`/${randomChannel.title}/${sample(randomChannel.videoIds)}`)
-  // return <Layout pageContext={{ channelTitle: randomChannel.title, videoId: sample(randomChannel.videoIds) }} />
+  isBrowser && navigate(`/${randomChannel.title}/${sample(randomChannel.videoIds)}`)
+
+  return <div>should redirect to a video page</div>
 }
 
 export default IndexPage
