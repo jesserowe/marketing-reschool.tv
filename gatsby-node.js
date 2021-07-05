812b5b50c36d7023b758874b11139f8539191274
Brandon@Effortless <brandon@effortlessdevelopment.com>
2021-07-05 16:38:27 -0400

    Add playlist ID's to channels.JSON & setup createPages in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,24 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const axios = require('axios');
 
-// You can delete this file if you're not using it
-// import channels from "./data/channels.json"
+exports.createPages = async ( actions: { { createPage } }) => {
+    const allPlaylists = []
+    require("./data/channels.json").forEach(({ playlistId, index }) => {
+        
+        await axios.get(`https://www.googleapis.com/youtube/v3/playlists?part=snippet&id=${playlistId}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`).then(playlistData => allPlaylists[index] = {
+            title: playlistData.items[0].snippet.localized.title,
+            playlistId
+        })
 
-exports.createPages = async ({ actions: { createPage } }) => {
-  require("./data/channels.json").forEach(channel => {
-    channel.videoIds.forEach(videoId => {
-      createPage({
-        path: `/${channel.title}/${videoId}`,
-        component: require.resolve("./src/templates/video-template.js"),
-        context: { channelTitle: channel.title, videoId },
-      })
+        await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`).then(playlistItems => {
+            allPlaylists[index].items = playlistItems.items.map(item => ({
+                videoId: item.id,
+                title: item.snippet.title
+            }))
     })
-  })
+
+    createPage({
+        path: $ `/${playlistId}/${videoId}`,
+        component: require.resolve("./src/templates/video-template.js"),
+        context: allPlaylists
+    })   
 }
